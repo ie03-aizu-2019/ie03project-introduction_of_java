e2eca5181b1b28274b77cb2be9248b080d995649
IsHYuhi <yuhiregend@icloud.com>
2019-04-16 14:01:03 +0900

    made axis class

--- Phase_1/DetectCrossingPoint.java
+++ Phase_1/DetectCrossingPoint.java
@@ -5,7 +5,7 @@ public class DetectCrossingPoint{
 
   public static void main(String[] args) {
 
-    int n, m, p, q, i;
+    int n, m, p, q, i, input_x, input_y;
     double detA, x, y;
     Scanner sc = new Scanner(System.in);
 
@@ -14,17 +14,20 @@ public class DetectCrossingPoint{
     p = sc.nextInt();
     q = sc.nextInt();
 
-    int [][] axis = new int[n][2];
+    Axis [] axis = new Axis[n];
 
     //p1,q1,p2,q2
-    int [][] axis4P = new int[n][2];
+    Axis [] axis4P = new Axis[n];
     int [] b = new int[m];
     int [] e = new int[m];
 
     //点の入力
     for(i = 0; i<n; i++){
-      axis[i][0] = sc.nextInt();
-      axis[i][1] = sc.nextInt();
+      // axis[i][0 = sc.nextInt();
+      // axis[i].y = sc.nextInt();
+      input_x = sc.nextInt();
+      input_y = sc.nextInt();
+      axis[i] = new Axis(input_x, input_y);
     }
 
     //経路を結ぶ点の入力
@@ -35,19 +38,17 @@ public class DetectCrossingPoint{
 
     //p1,q1,p2,q2を決定
     for(i = 0; i<m; i++){
-      axis4P[2*i][0] = axis[b[i]-1][0];
-      axis4P[2*i][1] = axis[b[i]-1][1];
-      axis4P[2*i+1][0] = axis[e[i]-1][0];
-      axis4P[2*i+1][1] = axis[e[i]-1][1];
+      axis4P[2*i] = axis[b[i]-1];
+      axis4P[2*i+1] = axis[e[i]-1];
     }
 
     // //４つの座標のテスト
     // for(i = 0; i<4; i++){
-    //   System.out.println(axis4P[i][0]+""+axis4P[i][1]);
+    //   System.out.println(axis4P[i].x+""+axis4P[i].y);
     // }
 
     //行列式の計算
-    detA = (axis4P[1][0]-axis4P[0][0])*(axis4P[2][1]-axis4P[3][1])+(axis4P[3][0]-axis4P[2][0])*(axis4P[1][1]-axis4P[0][1]);
+    detA = (axis4P[1].x-axis4P[0].x)*(axis4P[2].y-axis4P[3].y)+(axis4P[3].x-axis4P[2].x)*(axis4P[1].y-axis4P[0].y);
 
     //経路が平行（行列式が0）であればNA
     if(-(EPS)<=detA && detA<=EPS){
@@ -56,30 +57,26 @@ public class DetectCrossingPoint{
     }
 
     //媒介変数の計算
-    double s = ((axis4P[2][1]-axis4P[3][1])*(axis4P[2][0]-axis4P[0][0])+(axis4P[3][0]-axis4P[2][0])*(axis4P[2][1]-axis4P[0][1]))/detA;
-    double t = ((axis4P[0][1]-axis4P[1][1])*(axis4P[2][0]-axis4P[0][0])+(axis4P[1][0]-axis4P[0][0])*(axis4P[2][1]-axis4P[0][1]))/detA;
+    double s = ((axis4P[2].y-axis4P[3].y)*(axis4P[2].x-axis4P[0].x)+(axis4P[3].x-axis4P[2].x)*(axis4P[2].y-axis4P[0].y))/detA;
+    double t = ((axis4P[0].y-axis4P[1].y)*(axis4P[2].x-axis4P[0].x)+(axis4P[1].x-axis4P[0].x)*(axis4P[2].y-axis4P[0].y))/detA;
 
     //端点は含めないため0<s<1,0<t<1
     if((0<s && s<1) && (0<t && t<1)){
-      x = axis4P[0][0]+(axis4P[1][0]-axis4P[0][0])*s;
-      y = axis4P[0][1]+(axis4P[1][1]-axis4P[0][1])*s;
+      x = axis4P[0].x+(axis4P[1].x-axis4P[0].x)*s;
+      y = axis4P[0].y+(axis4P[1].y-axis4P[0].y)*s;
       System.out.printf("%.5f %.5f\n",x,y);
     }else{
       System.out.println("NA");
     }
+  }
+}
 
-    //入力できているかのテスト
-    //System.out.println(detA+ " " + s+ " " + t);
-    // for(i = 0; i<axis.length; i++){
-    //   for(int j = 0; j<axis[0].length; j++){
-    //     if(j==1)System.out.print(" ");
-    //     System.out.print(axis[i][j]);
-    //   }
-    //   System.out.println();
-    // }
-    //
-    // for(i = 0; i<b.length; i++){
-    //   System.out.println(b[i]+" "+e[i]);
-    // }
+class Axis{
+  public int x;
+  public int y;
+
+  public Axis(int x, int y){
+    this.x = x;
+    this.y = y;
   }
 }
