4093bb7be7a4eaa6884002e9e786c81002c02114
IsHYuhi <yuhiregend@icloud.com>
2019-04-22 00:53:49 +0900

    modified Detect program

--- Phase_1/ListCrossingPoint.java
+++ Phase_1/ListCrossingPoint.java
@@ -1,3 +1,25 @@
-public class ListCrossingPoint{
+public class ListCrossingPoint extends DetectCrossingPoint{
+  public static void main(String args[]){
+    System.out.println("this is List Program");
+    act(input());
+  }
 
+  public static void act(Segment [] seg){
+    Axis crossing = new Axis();
+    for(int i = 0; i<seg.length-1; i++){
+      for(int j =i+1; j<seg.length; j++){
+
+        //交差点検知のメソッド実行
+        crossing = detect(seg[i], seg[j]);
+
+        //交差点があれば座標を表示なければNA
+        if(crossing.x != -1 && crossing.y != -1){
+          System.out.printf("%.5f %.5f\n",crossing.x,crossing.y);
+        }else{
+          //System.out.println("NA");
+        }
+
+      }
+    }
+  }
 }
