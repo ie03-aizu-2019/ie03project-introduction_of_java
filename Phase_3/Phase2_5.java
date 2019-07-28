d01aea388e4e9493d79da6456b98df8091836a02
IsHYuhi <yuhiregend@icloud.com>
2019-07-28 23:39:24 +0900

    modify bags

--- Phase_3/Phase2_5.java
+++ Phase_3/Phase2_5.java
@@ -478,8 +478,7 @@ public class Phase2_5 extends Phase1_2 {
 
     double distance = data[end];
     if(distance == inf){
-      System.out.println("NA");
-      System.exit(0);
+      return null;
     }
     //System.out.printf("%.5f\n",distance);
     KPath k_path = ReverseSearchAndOutput(start, end, max, round(distance));
